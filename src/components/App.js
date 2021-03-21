a027a76c02911dabdbdd11d20ab678bd9323e4b2
KemPavel <pkazakov18@gmail.com>
2021-03-21 21:24:38 +0300

    links done

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/App.css';
+import LinksList from './LinksList';
+import CreateLink from './CreateLink';
+
+const App = () => {
+  return <CreateLink />;
+};
+
+export default App;
